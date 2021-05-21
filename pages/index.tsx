961320033dc8dc23b31d54ab51344d3c5e9dac93
Oleh Trukhan <oleg.truhan789@gmail.com>
2021-05-21 21:29:58 +0200

    added icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import { FunctionComponent } from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
+
+import { FunctionComponent } from 'react'
 
 const Home: FunctionComponent = () => {
   return (
@@ -7,9 +9,11 @@ const Home: FunctionComponent = () => {
       <Head>
         <title>Create Next App</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="weather-app.png" type="image/png" />
       </Head>
       <div>Next js</div>
       <div>test</div>
+      <Image src="/next-js.svg" height={60} width={60} />
     </div>
   )
 }
